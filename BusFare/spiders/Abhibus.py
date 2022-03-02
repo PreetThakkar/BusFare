facca605b2decee27b0fdcfb808673153a16267f
PreetThakkar <preetthakkar85@gmail.com>
2022-03-02 23:37:46 +0530

    Readme and comments

--- BusFare/spiders/Abhibus.py
+++ BusFare/spiders/Abhibus.py
@@ -6,31 +6,36 @@ from datetime import datetime, timedelta
 
 class AbhibusSpider(scrapy.Spider):
 	name = 'Abhibus'
+	# Starting point
 	start_urls = ['https://www.abhibus.com/bus_search/Delhi/344/Manali/1777/31-07-2020/O']
+	# Form Request path to gather bus fare
 	seatlayout_url = 'https://www.abhibus.com/seatlayout'
+	# Form Data required to get each travel agent's bus seat layout
 	form_data = {"rid": "", "sourceid": "344", "destination": "1777", "jdate": "", "concession": ""}
 
 	def parse(self, response):
+		# Gather data for next few(3) days
 		dates = []
 		today = datetime.today()
 		for i in range(3):
 			temp = today + timedelta(i)
 			dates.append(temp.strftime("%Y-%m-%d"))
-		print(f'\t{dates}')
+		# print(f'*****\nDates: \t{dates}*****\n')
 		for i in dates:
+			# Access path to list all the travel agents for particular date
 			print(f'https://www.abhibus.com/getonewayservices/{i}/344/1777')
 			request_main = scrapy.http.JsonRequest(f'https://www.abhibus.com/getonewayservices/{i}/344/1777', callback=self.parse_main_page)
-			request_main.cb_kwargs['date'] = i
+			request_main.cb_kwargs['date'] = i # pass argument with object
 			yield request_main
 
 	def parse_main_page(self, response, date):
 		raw = json.loads(response.body.decode())
 		service_list = raw['serviceDetailsList']
 		for agent in service_list:
-			rid = agent['serviceKey']
+			rid = agent['serviceKey'] # Identify unique key to pass as Form Data
 			self.form_data['rid'] = rid
 			self.form_data['jdate'] = date
-			# print('\t', rid, date)
+			# Gather seat layout information
 			agent_request =  scrapy.http.FormRequest(self.seatlayout_url, callback=self.parse_seat_layout, formdata=self.form_data)
 			agent_request.cb_kwargs['date'] = date
 			agent_request.cb_kwargs['rid'] = rid
@@ -39,6 +44,7 @@ class AbhibusSpider(scrapy.Spider):
 
 
 	def parse_seat_layout(self, response, date, rid, complete_detail):
+		# yeild the final output
 		item = self.format_fare(response.css('a.tooltip::attr(title)').extract())
 		yield {
 			'timestamp': str(datetime.now()),
--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # BusFare
 
-Specifically to learn Scrapy.
+This project is specifically to learn the Python framework *Scrapy*. The aim of the spider is to execute several requests and gather bus fares for a route.
+
+### Set up the project
+- Install python framework scrapy using ``` pip install scrapy ``` .
+- To run the spider, navigate to the ``` spiders ``` folder and run the following command ``` scrapy runspider Abhibus.py -o seatfare.json ``` .
+- You can terminate the process midway or wait for it's completion, in either case you will see data in the mentioned json file.
+- You are all set.
+
+### Output Data
+The output data will be list of dictionaries of the format
+```json
+{
+    "timestamp": "2021-03-09 16:44:15.092785", 
+    "date": "2021-03-09", 
+    "rid": "797805572", 
+    "travelerAgentName": "Xyz Travels", 
+    "seat": [
+        {"13": "1799"}, 
+        {"17": "1799"}, 
+        {"21": "1799"}, 
+        {"29": "1799"}, 
+    ]
+}
+```
